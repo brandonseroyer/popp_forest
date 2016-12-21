77265734b1bdba5bd82146b5a8a7166747d04fff
Brandon Seroyer <brandonseroyer@gmail.com>
2016-12-20 19:44:18 -0800

    create home, product, contact, about classes, models, views and controllers

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -13,9 +13,6 @@ Rails.application.config.action_controller.forgery_protection_origin_check = tru
 # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
 # Previous versions had false.
 
-# Require `belongs_to` associations by default. Previous versions had false.
-Rails.application.config.active_record.belongs_to_required_by_default = true
-
 # Do not halt callback chains when a callback returns false. Previous versions had true.
 
 # Configure SSL options to enable HSTS with subdomains. Previous versions had false.
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :home, only: [:index]
   resources :about, only: [:index]
-  resource :products, only: [:index]
+  resources :products, only: [:index]
   resources :contact , only: [:index]
 end

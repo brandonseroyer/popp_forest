e49371dc047eff62e2ceda37316e038712f6762f
Brandon Seroyer <brandonseroyer@Williams-Mac-mini.local>
2016-12-20 19:15:50 -0800

    add gems and layout framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+  resources :home, only: [:index]
+  resources :about, only: [:index]
+  resource :products, only: [:index]
+  resources :contact , only: [:index]
 end
